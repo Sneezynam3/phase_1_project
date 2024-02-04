19d8db597d076d4b6d46120c6a3f491eb07e0e7f
James Snell <vulpes3.1415@yahoo.com>
2024-02-04 08:53:11 -0700

    Added first image to html in order to have some element to change in the javascript

--- index.js
+++ index.js
@@ -16,17 +16,14 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Add mouseover event listener to #image-container
-  document.getElementById("image-container").addEventListener("mouseover", () => {
+  document.getElementById("imageContainer").addEventListener("mouseover", () => {
     // Fetch colors data from the server
     fetch('http://localhost:3000/colors')
       .then(response => response.json())
       .then(data => {
           console.log(data);
           data.forEach(image => {
-              console.log(colors.image);
-              const imgElement = document.createElement('img');
-              imgElement.src = colors.image;
-              document.body.appendChild(imgElement); // Append image to the body
+              
           });
       })
       .catch(error => {
