107c934b089d1ff371031adbf958482f841f1762
James Snell <vulpes3.1415@yahoo.com>
2024-02-04 13:39:19 -0700

    Changed text on tab. Changed css to center h1. Added empty array. Pushed every interval of forEach into said array. Made empty array for after.

--- index.js
+++ index.js
@@ -15,21 +15,29 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  let timeOutIds = []; 
+
   fetch('http://localhost:3000/colors')
     .then(response => response.json())
     .then(data => {
       console.log(data)
       const imgElement = document.getElementById("img");
+      
       imgElement.addEventListener("mouseover", () => {
         data.slice(0, 5).forEach((color, index) => { 
-          setTimeout(() => {
+          let timeOutId = setTimeout(() => {
             imgElement.src = `assets/${color.image}`;
           }, 1000 * (index + 1)); 
+
+          timeOutIds.push(timeOutId);
         });
       });
       imgElement.addEventListener("mouseout", () => {
+        timeOutIds.forEach(id => clearTimeout(id));
         imgElement.src = "../phase-1/assets/4-20copy.jpeg";
+        timeOutIds = []; // Clear the array for future use
       })
+      img
     })
     .catch(error => {
       console.error('Error fetching data:', error);
